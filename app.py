88f68850a4e2cc3a5f50c6c0d28cc02703d9e9d8
kora <radmehrkooshan@gmail.com>
2024-05-30 11:23:40 +0330

    app initilized

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user, login_required
+from forms import RegistrationForm, LoginForm
+from models import User, db
 
+# Initialize Flask app and extensions
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-import models  # Import models after initializing db to avoid circular import
+# User loader callback for login manager
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
